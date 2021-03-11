7f0cde575c5ed3b7bded1c0c96e37cb907c0ee88
snehal renushe <snehalrenushe15@gmail.com>
2021-03-11 21:40:53 +0530

    Array in C Programming

---
+++ Array/02 Array with Initialization and display all elements.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i[3] = {10,20,30}, No;
+
+    for(No = 0 ; No < 3 ; No++)
+    {
+        printf("\n Elements are : %d",i[No]);
+    }
+
+    getch();
+    return 0;
+}
