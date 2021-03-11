38eb6b365ee862f123112e6c09fb7b3c26201270
snehal renushe <snehalrenushe15@gmail.com>
2021-03-11 21:39:49 +0530

    Array in C

---
+++ Array/01 Array Without Initialization and display all elements.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i[3], No;
+
+    for(No = 0 ; No < 3 ; No++)
+    {
+        printf("\n Elements are : %d ",i[No]);
+    }
+
+    getch();
+    return 0;
+}
