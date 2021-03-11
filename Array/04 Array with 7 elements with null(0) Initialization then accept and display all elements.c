e4ea8b8139b14c0929b8ab8ed848354b471e8ef2
snehal renushe <snehalrenushe15@gmail.com>
2021-03-11 21:43:09 +0530

    Array in C Programming

---
+++ Array/04 Array with 7 elements with null(0) Initialization then accept and display all elements.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No[7] = {0}, i;
+
+    printf("\nEnter %d Elements for given array => ",i);
+    scanf("%d",&No[i]);
+
+    printf("\nEnter %d Elements for given array => ",i);
+    scanf("%d",&No[i]);
+
+    printf("\nEnter %d Elements for given array => ",i);
+    scanf("%d",&No[i]);
+
+    printf("\nEnter %d Elements for given array => ",i);
+    scanf("%d",&No[i]);
+
+    printf("\nEnter %d Elements for given array => ",i);
+    scanf("%d",&No[i]);
+
+    printf("\nEnter %d Elements for given array => ",i);
+    scanf("%d",&No[i]);
+
+    printf("\nEnter %d Elements for given array => ",i);
+    scanf("%d",&No[i]);
+
+
+    printf("\n %d Elements is : %d",i,No[i]);
+
+    printf("\n %d Elements is : %d",i,No[i]);
+
+    printf("\n %d Elements is : %d",i,No[i]);
+
+    printf("\n %d Elements is : %d",i,No[i]);
+
+    printf("\n %d Elements is : %d",i,No[i]);
+
+    printf("\n %d Elements is : %d",i,No[i]);
+
+    printf("\n %d Elements is : %d",i,No[i]);
+
+    getch();
+    return 0;
+}
