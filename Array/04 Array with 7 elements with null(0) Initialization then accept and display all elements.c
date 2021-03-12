7dbd80d0d1c74df5a692d79ff3319968ab77928a
snehal renushe <snehalrenushe15@gmail.com>
2021-03-12 18:57:06 +0530

    Array in C

--- Array/04 Array with 7 elements with null(0) Initialization then accept and display all elements.c
+++ Array/04 Array with 7 elements with null(0) Initialization then accept and display all elements.c
@@ -3,43 +3,43 @@
 
 int main()
 {
-    int No[7] = {0}, i;
+    int No[7] = {0}, i = 0;
 
-    printf("\nEnter %d Elements for given array => ",i);
-    scanf("%d",&No[i]);
+    printf("\nEnter 1st Elements for given array => ");
+    scanf("%d",&No[0]);
 
-    printf("\nEnter %d Elements for given array => ",i);
-    scanf("%d",&No[i]);
+    printf("\nEnter 2nd Elements for given array => ");
+    scanf("%d",&No[1]);
 
-    printf("\nEnter %d Elements for given array => ",i);
-    scanf("%d",&No[i]);
+    printf("\nEnter 3rd Elements for given array => ");
+    scanf("%d",&No[2]);
 
-    printf("\nEnter %d Elements for given array => ",i);
-    scanf("%d",&No[i]);
+    printf("\nEnter 4th Elements for given array => ");
+    scanf("%d",&No[3]);
 
-    printf("\nEnter %d Elements for given array => ",i);
-    scanf("%d",&No[i]);
+    printf("\nEnter 5th Elements for given array => ");
+    scanf("%d",&No[4]);
 
-    printf("\nEnter %d Elements for given array => ",i);
-    scanf("%d",&No[i]);
+    printf("\nEnter 6th Elements for given array => ");
+    scanf("%d",&No[5]);
 
-    printf("\nEnter %d Elements for given array => ",i);
-    scanf("%d",&No[i]);
+    printf("\nEnter 7th Elements for given array => ");
+    scanf("%d",&No[6]);
 
 
-    printf("\n %d Elements is : %d",i,No[i]);
+    printf("\n %d Elements is : %d",++i,No[0]);
 
-    printf("\n %d Elements is : %d",i,No[i]);
+    printf("\n %d Elements is : %d",++i,No[1]);
 
-    printf("\n %d Elements is : %d",i,No[i]);
+    printf("\n %d Elements is : %d",++i,No[2]);
 
-    printf("\n %d Elements is : %d",i,No[i]);
+    printf("\n %d Elements is : %d",i++,No[3]);
 
-    printf("\n %d Elements is : %d",i,No[i]);
+    printf("\n %d Elements is : %d",i++,No[4]);
 
-    printf("\n %d Elements is : %d",i,No[i]);
+    printf("\n %d Elements is : %d",i++,No[5]);
 
-    printf("\n %d Elements is : %d",i,No[i]);
+    printf("\n %d Elements is : %d",i++,No[6]);
 
     getch();
     return 0;
