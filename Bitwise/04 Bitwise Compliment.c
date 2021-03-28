0bc3d2740fb6f326758cf008d0935500232d7bb7
snehal renushe <snehalrenushe15@gmail.com>
2021-03-28 22:48:57 +0530

    Compliment

---
+++ Bitwise/04 Bitwise Compliment.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int No1 = 0 , Res = 0;
+
+    printf("\nEnter a Number =>" );
+    scanf("%d",&No1);
+
+    Res = ~No1;
+
+    printf("\n Bitwise Compliment  Between %d ^ %d = %d.",No1,Res);
+
+    getch();
+    return 0;
+}
