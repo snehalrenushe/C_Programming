0a96b3a1824baf82dbde61dc7ce10a978e97d143
snehal renushe <snehalrenushe15@gmail.com>
2022-01-01 23:13:22 +0530

    Array demo

---
+++ Array/20 Demo.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int arr[3] = {21,43,54};
+
+    int x = 0;
+
+    x = arr[2] + 21 + arr[0];
+
+    x++;
+
+    printf("%d",x);
+    getch();
+
+    return 0;
+}
